4e53759400ab9904022529d5cae5c76f43723444
GhostMaster69-dev <rathore6375@gmail.com>
2022-05-21 23:38:06 +0530

    vince: SuperiorOS bringup.
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- superior_vince.mk
+++ superior_vince.mk
@@ -27,28 +27,25 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 # Inherit from vince device
 $(call inherit-product, device/xiaomi/vince/device.mk)
 
-# Inherit some common Octavi stuff.
-$(call inherit-product, vendor/octavi/config/common_full_phone.mk)
+# Inherit some common SuperiorOS stuff.
+$(call inherit-product, vendor/superior/config/common.mk)
 
-# Include Octavi specific sepolicy
--include device/octavi/sepolicy/qcom/sepolicy.mk
+# Include SuperiorOS specific sepolicy
+-include device/superior/sepolicy/qcom/sepolicy.mk
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Custom stuff
 TARGET_FACE_UNLOCK_SUPPORTED := true
-USE_PIXEL_CHARGER := true
-
-# Build Status
-OCTAVI_BUILD_TYPE := Official
+TARGET_INCLUDE_PIXEL_CHARGER := true
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := xiaomi
 PRODUCT_DEVICE := vince
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi 5 Plus
-PRODUCT_NAME := octavi_vince
+PRODUCT_NAME := superior_vince
 PRODUCT_SYSTEM_NAME := vince
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
